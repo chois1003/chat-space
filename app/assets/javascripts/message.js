99e6f296ccc2f865bcd0c2b93b58e938926d9be5
chois1003 <choe2ss@gmail.com>
2020-01-14 21:23:07 +0900

    コードの修正：セミコロン削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -60,9 +60,9 @@ $('#new_message').on('submit', function(e){
   })  
   .fail(function() {
     alert("メッセージ送信に失敗しました");
-  });  
+  })  
   .always(function(){
     $(".form__submit").removeAttr("disabled");
-    })
+  })
 })
 });
