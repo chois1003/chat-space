a999aeb6edd64a8ebb5539a0570fc9b98729e1b4
chois1003 <choe2ss@gmail.com>
2020-01-16 17:59:58 +0900

    自動更新機能実装での修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
   last_message_id = $('.message:last').data("message-id");
-  console.log(last_message_id);
+  
   function buildHTML(message){
    if ( message.image ) {
      var html =
@@ -85,21 +85,19 @@ $(function(){
         var insertHTML = '';
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
+          insertHTML = buildHTML(message)
         });
         //メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-        $("#new_message")[0].reset();
-        $(".form__submit").prop("disabled", false);
+        // $("#new_message")[0].reset();
+        // $(".form__submit").prop("disabled", false);
       }
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
-  setInterval(reloadMessages, 7000);
-
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
